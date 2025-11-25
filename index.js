5877a131a5b358ad6993f976c51b8922549c5869
Julien <julien3101@hotmail.com>
2025-11-25 10:59:35 +0100

    change log
    a la place d'afficher le port d'écoute, affiche le lien

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(express.json());
 connectDB();
 app.listen(PORT, (error) =>{
     if(!error)
-        console.log("Server is Successfully Running, and App is listening on port "+ PORT);
+        console.log("Server is Successfully Running, and App is listening on http://localhost:"+ PORT);
     else 
         console.log("Error occurred, server can't start", error);
     } 
