66036d1dd52d071224126f8a7167daddb4079123
Julien <julien3101@hotmail.com>
2025-11-25 10:42:55 +0100

    Connect MongoDB Atlas
    ajout du code avec mongoose pour se connecter a mongoDB

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
 const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
 
 const app = express();
 const PORT = 3000;
 
+//test connection a la base de données
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('MongoDB Connected...');
+  } catch (error) {
+    console.error(error.message);
+    process.exit(1);
+  }
+};
+
+app.use(express.json());
+
+connectDB();
 app.listen(PORT, (error) =>{
     if(!error)
         console.log("Server is Successfully Running, and App is listening on port "+ PORT);
     else 
         console.log("Error occurred, server can't start", error);
-    }
+    } 
 );
